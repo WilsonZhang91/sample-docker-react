823b61f921206b6cb5a1ff7b659529321ec62eae
Wilson Zhang <wilsonzhang91@hotmail.com>
2023-01-03 23:03:51 +0700

    comment deploy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Docker Simple App
+          Learn React
         </a>
       </header>
     </div>
